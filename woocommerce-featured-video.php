62abfd8eb44a64e077c19cef2d0a78b03b4a80be
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-02-06 13:27:06 +0530

    plugin is assign

--- woocommerce-featured-video.php
+++ woocommerce-featured-video.php
@@ -1,1 +1,36 @@
- 
+<?php
+/**
+ * Plugin Name: WooCommerce Featured Video
+ * Plugin URI: https://yourwebsite.com/
+ * Description: Adds a featured video option for WooCommerce products.
+ * Version: 1.0.0
+ * Author: Professor
+ * Author URI: https://yourwebsite.com/
+ * License: GPL2
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Define plugin constants
+define('WCFV_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('WCFV_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+// Include necessary files
+require_once WCFV_PLUGIN_DIR . 'includes/admin-settings.php';
+require_once WCFV_PLUGIN_DIR . 'includes/video-display.php';
+
+// Activation Hook
+function wcfv_activate() {
+    // Activation logic here
+}
+register_activation_hook(__FILE__, 'wcfv_activate');
+
+// Deactivation Hook
+function wcfv_deactivate() {
+    // Deactivation logic here
+}
+register_deactivation_hook(__FILE__, 'wcfv_deactivate');
+
